24d83d34faa980184add72372d3b62eee693639b
mkhan42 <michallekhan9@gmail.com>
2022-05-24 19:41:02 -0400

    Have one enemy falling down screen

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let score = document.querySelector('#score');
 let cat;
 let fish;
 let fishArr = [];
+let fishXPos = Math.floor(Math.random() * 100);
+let fishYPos = Math.floor(Math.random() * 100);
+let enemy;
+
 
 canvas.style.background = '#571FF1';
 canvas.style.border = '8px solid #54123B';
@@ -14,16 +18,19 @@ catImage.src = "images/grinning-cat-face.png";
 const fishImage = new Image();
 fishImage.src = 'images/fish.png';
 
+const enemyImage = new Image();
+enemyImage.src = 'images/banana.png';
+
 class Player {
-    constructor(url, x, y, width, height) {
-    this.url = url;
+    constructor(x, y, width, height, speed) {
     this.x = x;
     this.y = y;
     this.height = height;
     this.width = width;
     this.alive = true;
+    this.speed = speed;
     this.render = function () {
-      ctx.drawImage(this.url, this.x, this.y, this.width, this.height);
+      ctx.drawImage(catImage, this.x, this.y, this.width, this.height);
     };
     };
   }
@@ -38,31 +45,71 @@ class Player {
 
     this.speed = speed;
 
-    this.dx = 1 * this.speed;
-    this.dy = 1 * this.speed;
+    // this.dx = 1 * this.speed;
+    // this.dy = 1 * this.speed;
     this.render = function () {
       ctx.drawImage(fishImage, this.x, this.y, this.width, this.height);
     };
     };
-
   }
 
-  let fishXPos = Math.floor(Math.random() * 100);
-  let fishYPos = Math.floor(Math.random() * 100);
+  // class Enemy {
+  //   constructor(x, y, color, width, height, speed){
+  //       this.x = x;
+  //       this.y = y;
+  //       this.color = color;
+  //       this.height = height;
+  //       this.width = width; 
+  //       this.alive = true;
+  //       this.speed = speed;
+
+  //       // this.dx = 1 * this.speed;
+  //       // this.dy = 1 * this.speed;
+
+
+  //   }
+
+  //   render() {
+  //       ctx.fillStyle = this.color;
+  //       ctx.fillRect(this.x, this.y, this.width, this.height)
+  //   }
+  // }
+
+let enemyXPos = 0;
+let enemyYPos = 0;
+let enemyXSpeed = 5;
+let enemyYSpeed = 5;
+
+  class Enemy {
+    constructor(x, y, width, height, speed) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+    this.alive = true;
+    this.speed = speed;
+    this.render = function () {
+      ctx.drawImage(enemyImage, this.x, this.y, this.width, this.height);
+    };
+    };
+
+    move() {
+      enemyXPos += enemyXSpeed;
+    }
+  }
+
+
 
 window.addEventListener("DOMContentLoaded", function(e){
-  cat = new Player(catImage, 0, 130, 20, 20);
-  ctx.imageSmoothingEnabled = false;
-  //const playGame = setInterval(gameLoop, 100);
+  cat = new Player(0, 130, 20, 20);
   for(let i = 0; i < 30; i++) {
     const fishXPos = Math.floor(Math.random() * 100);
     const fishYPos = Math.floor(Math.random() * 100);
     fish = new Fishies(fishXPos * 2.8, fishYPos, 15, 15);
     fishArr.push(fish);
     console.log(fish)
-    //console.log(fishArr);
   }
-      //console.log(fish)
+  ctx.imageSmoothingEnabled = false;
 
 })
 
@@ -85,21 +132,30 @@ window.addEventListener("DOMContentLoaded", function(e){
   
   }
 
-  
 
+
+console.log(score);
+
+function enemyMove() {
+  enemyYPos += enemyYSpeed;
+
+}
   
   function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     cat.render();
 
-    fishArr.forEach(fish => {
-      fish.render();
+      fishArr.forEach(fish => {
+        fish.render();
     })
+    enemy = new Enemy(enemyXPos, enemyYPos, 15, 15)
+    enemy.render();
+    enemyMove();
   }
 
-  let varName = setInterval(gameLoop, 100);
 
+  let varName = setInterval(gameLoop, 100);
   document.addEventListener("keydown", move)
 
 
