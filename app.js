a6ede749e03c3e2ac812a928a763731efbc417e8
mkhan42 <michallekhan9@gmail.com>
2022-05-31 15:19:32 -0400

    test

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ let gameStart = () => {
     enemyFoods.push(food);
   }
 
-  //drawing the enemies array using a forEach statement.
+  //drawing the enemies array using a forEach statem
   function drawEnemyFood() {
     enemyFoods.forEach(function(food) {
       ctx.drawImage(food.enemyImage, food.foodXPos, food.foodYPos, food.foodSize, food.foodSize)
